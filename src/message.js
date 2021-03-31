3ffd050b4a9bb5295dfec6a017c45689296f4271
salman898 <s.t.khan701@gmail.com>
2021-03-31 16:12:10 +0500

    space added

--- src/message.js
+++ src/message.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export function Message (props){
     return(
-        <h3>Value of variable is: {props.counter}</h3>
+        <h3>Value of variable is : {props.counter}</h3>
     )
 }

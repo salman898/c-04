0fd0811c1441318b3a2bcdd25db723f53a9f4153
salman898 <s.t.khan701@gmail.com>
2021-03-31 16:02:07 +0500

    upload on git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import React, {useState} from 'react';
+// import {Message } from './message.js'
+// import './App.css';
+
+// function App() {
+//   let [count, setCount] = useState(1)
+//   let [isMorning, setMorning] = useState(true)
+//   return (
+//     <div className={'box ${isMorning ? 'dayLight' : ''}'}>
+//       <h1>Have A Good {isMorning ? 'Morning' : 'Night'}</h1>
+//       <Message counter={count} />
+//       <br />
+//       <button onClick={
+//         ()=> setCount(count++)
+//       }>
+//         Update Counter</button>
+
+//       <button onClick={()=>setMorning(!isMorning)}> Update Day </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+import { Message } from './message.js'
 import './App.css';
 
-function App() {
+export default function App() {
+
+  let [count, setCount] = useState(1);
+  let [isMorning, setMorning] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+
+      <Message counter={count} />
+      <br />
+
+      <button onClick={() => setCount(count + 1)}>
+        Update Counter
+      </button>
+
+      <button onClick={() => setMorning(!isMorning)}> 
+        Update Day
+      </button>
+
     </div>
   );
 }
-
-export default App;
---
+++ src/message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Message (props){
+    return(
+        <h3>Value of variable is: {props.counter}</h3>
+    )
+}
